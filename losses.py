b2be27efe3308f308e4355e6335487d77bc78ff0
aymanelghotni <48604489+aymanelghotni@users.noreply.github.com>
2022-01-25 03:31:48 +0200

    Fixed bugs

--- losses.py
+++ losses.py
@@ -1,9 +1,9 @@
 def jaccard(output,target,eps=1e-5):
   target=target.float()
-  intersection=((output*target).sum())+eps
-  union=(output.sum()+target.sum()-intersection)+eps
-  return 1-(intersection/union)
-  
+  intersection=((output*target).sum())
+  union=(output.sum()+target.sum()-intersection)
+  return 1-((intersection+eps)/(union+eps))
+
 def softmax_jaccard(output, target, eps=1e-5):
     target_0=(target==0).float()
     target_255=(target==255).float()
@@ -12,10 +12,13 @@ def softmax_jaccard(output, target, eps=1e-5):
     jaccard_0=jaccard(output_0,target_0)
     jaccard_1=jaccard(output_1,target_255)
     return ((jaccard_0+jaccard_1)/2.)
+
 def sigmoid_jaccard(output, target, eps=1e-5):
-    target_0=(target==0).float()
-    target_255=(target==255).float()
-    output_0=1-output
+    target_0=(target==0).float() #=0
+    target_255=(target==255).float() #255*2
+    output_0=1-output #-255*2
     jaccard_0=jaccard(output_0,target_0)
+
     jaccard_1=jaccard(output,target_255)
+
     return ((jaccard_0+jaccard_1)/2.)
