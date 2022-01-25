92c30cffae7c26bc0c273ab7933ea90c86397a7f
Mostafa Hakam <48574002+MostafaHakam98@users.noreply.github.com>
2022-01-25 03:11:31 +0200

    Creating path maker
    Creates text files containing paths of data in training and validation

---
+++ PathFileMaker.py
@@ -0,0 +1,25 @@
+class fileMaker:
+
+    def __init__(self, textfile_train="", textfile_valid="", path_images_train="", path_images_valid=""):
+        self.textfile_train = textfile_train
+        self.textfile_valid = textfile_valid
+        self.path_images_train = path_images_train
+        self.path_images_valid = path_images_valid
+        self.create_list_files()
+
+    def create_list_files(self):
+        a = open(self.textfile_train, "w")
+
+        for path, subdirs, files in os.walk(self.path_images_train):
+            for file in files:
+                f = os.path.join(file)
+                if str(f)[-4:] == ".jpg":
+                    a.write(str(f) + os.linesep)
+
+        b = open(self.textfile_valid, "w")
+
+        for path, subdirs, files in os.walk(self.path_images_valid):
+            for file in files:
+                f = os.path.join(file)
+                if str(f)[-4:] == ".jpg":
+                    b.write(str(f) + os.linesep)
