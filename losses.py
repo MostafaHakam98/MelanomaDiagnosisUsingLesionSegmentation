69cc2377aab91fe122ac61f4306dad001212fadb
aymanelghotni <48604489+aymanelghotni@users.noreply.github.com>
2022-01-25 02:43:06 +0200

    Create losses.py

---
+++ losses.py
@@ -0,0 +1,14 @@
+def jaccard(output,target,eps=1e-5):
+  target=target.float()
+  intersection=((output*target).sum())+eps
+  union=(output.sum()+target.sum()-intersection)+eps
+  return 1-(intersection/union)
+  
+def sigmoid_jaccard(output, target, eps=1e-5):
+    target_0=(target==0).float()
+    target_255=(target==255).float()
+    output_0=output[:,0,...]
+    output_1=output[:,1,...]
+    jaccard_0=jaccard(output_0,target_0)
+    jaccard_1=jaccard(output_1,target_255)
+    return ((jaccard_0+jaccard_1)/2.)
