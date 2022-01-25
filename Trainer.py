d713c9a3371b8a49ba83d655442ce6eec6ac8d13
Mostafa Hakam <48574002+MostafaHakam98@users.noreply.github.com>
2022-01-26 01:26:04 +0200

    Fixed unfound folders errors

--- Trainer.py
+++ Trainer.py
@@ -17,34 +17,20 @@ from torchvision.transforms import transforms
 from pynvml import *
 
 
-
 def to_np(x):
     return x.data.cpu().numpy()
 
 class BatchResult(NamedTuple):
-    """
-    Represents the result of training for a single batch: the loss
-    and score of the batch.
-    """
     loss: float
     score: float
 
 
 class EpochResult(NamedTuple):
-    """
-    Represents the result of training for a single epoch: the loss per batch
-    and accuracy on the dataset (train or test).
-    """
     losses: List[float]
     score: float
 
 
 class FitResult(NamedTuple):
-    """
-    Represents the result of fitting a model for multiple epochs given a
-    training and test (or validation) set.
-    The losses are for each batch and the accuracies are per epoch.
-    """
     num_epochs: int
     train_loss: List[float]
     train_acc: List[float]
@@ -53,13 +39,6 @@ class FitResult(NamedTuple):
     best_score: float
 
 class Trainer:
-    """
-    A class abstracting the various tasks of training models.
-    Provides methods at multiple levels of granularity:
-    - Multiple epochs (fit)
-    - Single epoch (train_epoch/test_epoch)
-    - Single batch (train_batch/test_batch)
-    """
     def __init__(self,
                  model,
                  loss_fn,
@@ -72,15 +51,7 @@ class Trainer:
                  tensorboard_log_images=True,
                  experiment_prefix=None
                  ):
-        """
-        Initialize the trainer.
-        :param model: Instance of the model to train.
-        :param loss_fn: The loss function to evaluate with.
-        :param optimizer: The optimizer to train with.
-        :param device: torch.device to run training on (CPU or GPU).
-        :param callbacks: A list of strings defining methods to be called at the start of each epoch
-        :param tensorboard_logger: tensordboard logger.
-        """
+
         self.tensorboard_logger = tensorboard_logger
 
         if experiment_prefix is None:
@@ -108,21 +79,7 @@ class Trainer:
             best_score=None,
             current_epoch=0,
             print_every=1, **kw) -> FitResult:
-        """
-        Trains the model for multiple epochs with a given training set,
-        and calculates validation loss over a given validation set.
-        :param dl_train: Dataloader for the training set.
-        :param dl_test: Dataloader for the test set.
-        :param num_epochs: Number of epochs to train for.
-        :param checkpoints: Whether to save model to file every time the
-            test set accuracy improves. Should be a string containing a
-            filename without extension.
-        :param empty cache: To empty cuda cache at the start of each epoch
-        :param early_stopping: Whether to stop training early if there is no
-            test loss improvement for this number of epochs.
-        :param print_every: Print progress every this number of epochs.
-        :return: A FitResult object containing train and test losses per epoch.
-        """
+
         actual_num_epochs = 0
         train_loss, train_acc, test_loss, test_acc = [], [], [], []
 
@@ -156,8 +113,14 @@ class Trainer:
             elif epoch_test_res.score > best_score:
                 best_score = epoch_test_res.score
                 if checkpoints is not None:
-                    torch.save(self.model, checkpoints+'best_models/'+self.model.__class__.__name__.split('_')[0]+'_'+str(best_score)+'_'+str(epoch))
-                    print("**** Checkpoint saved ****")
+                    if not os.path.exists(checkpoints):
+                        os.makedirs(checkpoints)
+
+                    if not os.path.exists(checkpoints + 'best_models/'):
+                        os.makedirs(checkpoints + 'best_models/')
+
+                    torch.save(self.model, checkpoints + 'best_models/'+self.model.__class__.__name__.split('_')[0]+'_'+str(best_score)+'_'+str(epoch))
+                    print("**** New Best Weights Saved ****")
                 epochs_without_improvement = 0
 
             else:
@@ -165,7 +128,13 @@ class Trainer:
                     print("Early stopping after %s with out improvement" % epochs_without_improvement)
                     break
                 if checkpoints is not None:
-                    print("****Intermediate model saved ****")
+                    if not os.path.exists(checkpoints):
+                        os.makedirs(checkpoints)
+
+                    if not os.path.exists(checkpoints + 'intermediate_models/'):
+                        os.makedirs(checkpoints + 'intermediate_models/')
+
+                    print("**** Intermediate Weights Saved ****")
                     torch.save(self.model, checkpoints+'intermediate_models/'+self.model.__class__.__name__.split('_')[0]+'_'+str(epoch)+'_'+str(epoch_test_res.score))
                 epochs_without_improvement += 1
 
@@ -184,35 +153,14 @@ class Trainer:
             param_group['lr'] = self.lr*decayed
     
     def train_epoch(self, dl_train: DataLoader, **kw) -> EpochResult:
-        """
-        Train once over a training set (single epoch).
-        :param dl_train: DataLoader for the training set.
-        :param kw: Keyword args supported by _foreach_batch.
-        :return: An EpochResult for the epoch.
-        """
         self.model.train()  
         return self._foreach_batch(self.device_info,dl_train, self.train_batch, **kw)
 
     def test_epoch(self, dl_test: DataLoader, **kw) -> EpochResult:
-        """
-        Evaluate model once over a test set (single epoch).
-        :param dl_test: DataLoader for the test set.
-        :param kw: Keyword args supported by _foreach_batch.
-        :return: An EpochResult for the epoch.
-        """
         self.model.eval()  # set evaluation (test) mode
         return self._foreach_batch(self.device_info,dl_test, self.test_batch, **kw)
 
     def train_batch(self, index, batch_data) -> BatchResult:
-        """
-        Runs a single batch forward through the model, calculates loss,
-        preforms back-propagation and uses the optimizer to update weights.
-        :param batch: A single batch of data  from a data loader (might
-            be a tuple of data and labels or anything else depending on
-            the underlying dataset.
-        :return: A BatchResult containing the value of the loss function and
-            the number of correctly classified samples in the batch.
-        """
         X, y = batch_data
         ############TO BE CONSIDERED##############
         if self.tensorboard_logger and self.tensorboard_log_images:
@@ -241,14 +189,6 @@ class Trainer:
         return BatchResult(loss, score)
 
     def test_batch(self, index, batch_data) -> BatchResult:
-        """
-        Runs a single batch forward through the model and calculates loss.
-        :param batch: A single batch of data  from a data loader (might
-            be a tuple of data and labels or anything else depending on
-            the underlying dataset.
-        :return: A BatchResult containing the value of the loss function and
-            the number of correctly classified samples in the batch.
-        """
         with torch.no_grad():
             X, y = batch_data
             if isinstance(X, tuple) or isinstance(X, list):
@@ -269,7 +209,6 @@ class Trainer:
 
     @staticmethod
     def _print(message, verbose=True):
-        """ Simple wrapper around print to make it conditional """
         if verbose:
             print(message)
 
@@ -277,10 +216,6 @@ class Trainer:
     def _foreach_batch(device_info,dl: DataLoader,
                        forward_fn: Callable[[Any], BatchResult],
                        verbose=True, max_batches=None) -> EpochResult:
-        """
-        Evaluates the given forward-function on batches from the given
-        dataloader, and prints progress along the way.
-        """
         losses = []
         num_samples = len(dl.sampler)
         num_batches = len(dl.batch_sampler)
