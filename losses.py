d817b051c0e0ca1e9ce9367e8ff4aad86f59df21
aymanelghotni <48604489+aymanelghotni@users.noreply.github.com>
2022-01-25 02:52:50 +0200

    Added sigmoid jaccard

--- losses.py
+++ losses.py
@@ -4,11 +4,18 @@ def jaccard(output,target,eps=1e-5):
   union=(output.sum()+target.sum()-intersection)+eps
   return 1-(intersection/union)
   
-def sigmoid_jaccard(output, target, eps=1e-5):
+def softmax_jaccard(output, target, eps=1e-5):
     target_0=(target==0).float()
     target_255=(target==255).float()
     output_0=output[:,0,...]
     output_1=output[:,1,...]
     jaccard_0=jaccard(output_0,target_0)
     jaccard_1=jaccard(output_1,target_255)
+    return ((jaccard_0+jaccard_1)/2.)
+def sigmoid_jaccard(output, target, eps=1e-5):
+    target_0=(target==0).float()
+    target_255=(target==255).float()
+    output_0=1-output
+    jaccard_0=jaccard(output_0,target_0)
+    jaccard_1=jaccard(output,target_255)
     return ((jaccard_0+jaccard_1)/2.)
