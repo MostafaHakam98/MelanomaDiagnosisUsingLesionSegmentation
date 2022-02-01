708380c01c5f1b9e7c3f6313e04d5c352ca47279
Mostafa Hakam <48574002+MostafaHakam98@users.noreply.github.com>
2022-02-01 16:26:03 +0200

    Implementing Tester

---
+++ Tester.py
@@ -0,0 +1,28 @@
+import tqdm
+
+
+class test:
+    def __init__(self, model, loss_fn, objective_metric):
+        self.sum = 0
+        self.model = model
+        self.loss_fn = loss_fn
+        self.objective_metric = objective_metric
+
+    def computer_avg_score(self, test_set):
+        for i in tqdm.tqdm(range(len(test_set))):
+            img = test_set[i][0]
+            label = test_set[i][1]
+
+            image_tensor = torch.unsqueeze(img, 0)
+            image_tensor = image_tensor.to(device)
+
+            label_tensor = torch.unsqueeze(label, 0)
+            label_tensor = label_tensor.to(device)
+
+            pred = self.model(image_tensor)
+            loss = self.loss_fn(pred, label_tensor)
+            score = self.objective_metric(pred, label_tensor)
+
+            self.sum += float(score.item())
+
+        return sum / len(test_set)
